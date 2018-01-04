5355d742fe0237e1837769193f38c2e6610af555
Eric Chan <eric.chan@intellihr.com.au>
2018-01-05 00:16:24 +1000

    Hack

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './link';
+export { createOperation, makePromise, toPromise, fromPromise } from './linkUtils';
+export * from './types';
+
+import Observable from 'zen-observable';
+
+export { Observable };
